88bff8b5eb2ebf6b66a71ac11ee850704d4d7ffd
SzabolcsBodi756 <bodisz@kkszki.hu>
2025-10-03 10:31:57 +0200

    ready

---
+++ Test/Book.cs
@@ -0,0 +1,17 @@
+﻿namespace Test
+{
+    public class Book
+    {
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime RelesDate { get; set; }
+
+
+
+    }
+}
---
+++ Test/Connect.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using Mysqlx.Connection;
+
+namespace Test
+{
+    public class Connect
+    {
+
+        public MySqlConnection ? Connection;
+
+        private string _host;
+        private string _db;
+        private string _user;
+        private string _password;
+
+        private string ConnectionString;
+
+        public Connect()
+        {
+            _host = "localhost";
+            _db = "libary";
+            _user = "root";
+            _password = "";
+
+            ConnectionString = $"SERVER = {_host}; DATABASE = {_db}; UID = {_user}; PASSWORD = {_password}; SslMode = none";
+
+            Connection = new MySqlConnection(ConnectionString);
+        }
+    }
+}
